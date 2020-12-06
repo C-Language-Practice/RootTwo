e8265d142846393ef82935448f49f7e48a090236
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-06 14:04:12 +0100

    done

---
+++ RootTwo/RootTwo/RootTwo.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double num = 1;
+	double diff = 0.1;
+
+	for (int i = 0; i < 10; i++)
+	{
+		while (num * num < 2)
+		{
+			num = num + diff;
+		}
+		num = num - diff;
+		diff = diff * 0.1;
+	}
+	printf_s("%.10f", num);
+
+	return 0;
+}
